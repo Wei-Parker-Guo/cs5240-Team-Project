0175c40bb0047a5b639d2f6b5eb00e94743810d9
Wei Guo <wei.parker.guo.sg@gmail.com>
2022-11-08 20:54:19 +0800

    [BugFix] Assignment of I1 and I2 to I.

--- main.py
+++ main.py
@@ -45,14 +45,21 @@ def simulate_views(I1, I2, t1, t2, ts, img_f):
         q2 = q2.reshape(-1, 3)[:, :2]
         q1 = np.round(denormalise(q1, I1.shape[1], I1.shape[0])).astype(int)
         q2 = np.round(denormalise(q2, I2.shape[1], I2.shape[0])).astype(int)
+        print('q1')
+        print(q1[:, 0])
+        print('q2')
+        print(q2[:, 0])
         # generate view I
         I = np.zeros(I1.shape, dtype=np.uint8)
+        q10 = q1.T[0][0]
+        q20 = q2.T[0][0]
+        print(q10, q20)
         q1 = q1[(q1[:, 1] < I.shape[0]) & (q1[:, 1] >= 0)]
         q1 = q1[(q1[:, 0] < I.shape[1]) & (q1[:, 0] >= 0)]
         q2 = q2[(q2[:, 1] < I.shape[0]) & (q2[:, 1] >= 0)]
         q2 = q2[(q2[:, 0] < I.shape[1]) & (q2[:, 0] >= 0)]
-        I[q1.T[1], q1.T[0], :] = I1[:, :q1.shape[0] // I1.shape[0], :].reshape(-1, 3)
-        I[q2.T[1], q2.T[0], :] = I2[:, I2.shape[1] - q2.shape[0] // I2.shape[0]:, :].reshape(-1, 3)
+        I[q1.T[1], q1.T[0], :] = I1[q1.T[1], q1.T[0] - q10, :].reshape(-1, 3)
+        I[q2.T[1], q2.T[0], :] = I2[q2.T[1], q2.T[0] - q20, :].reshape(-1, 3)
         PI = Image.fromarray(I)
         PI.save('./out/' + img_f + '-t={:.2f}.jpg'.format(t))
 
@@ -67,8 +74,8 @@ if __name__ == '__main__':
     # save cropped images
     # I1 = original_img[:, :ow * 2 // 3, :]
     # I2 = original_img[:, ow // 3:, :]
-    I1 = np.asarray(Image.open('./test_pic/0.pic.jpg'))
-    I2 = np.asarray(Image.open('./test_pic/10.pic.jpg'))
+    I1 = np.asarray(Image.open('./test_pic/2-I1.jpg'))
+    I2 = np.asarray(Image.open('./test_pic/2-I2.jpg'))
     PI1 = Image.fromarray(I1)
     PI2 = Image.fromarray(I2)
     PI1.save('./out/' + img_f + '-I1.jpg')
@@ -76,7 +83,8 @@ if __name__ == '__main__':
 
     # simulate theta generated image at 10 locations
     theta_1 = 0
-    theta_2 = math.pi / 180 * 10
+    # theta_2 = math.pi / 180 * 29.225
+    theta_2 = math.pi / 180 * 25
     thetas = np.arange(theta_1, theta_2+1e-5, (theta_2 - theta_1) / 20)
     simulate_views(I2, I1, theta_1, theta_2, thetas, img_f)
 
